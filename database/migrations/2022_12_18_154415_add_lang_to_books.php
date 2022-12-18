9261f8512c6dd62c00b41fd99f7ae29dff6ab659
DariSarsen <113712560+DariSarsen@users.noreply.github.com>
2022-12-19 02:36:00 +0600

    .

---
+++ database/migrations/2022_12_18_154415_add_lang_to_books.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->text('description_kz')->nullable();
+            $table->text('description_en')->nullable();
+            $table->text('description_ru')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('description_kz');
+            $table->dropColumn('description_en');
+            $table->dropColumn('description_ru');
+        });
+    }
+};
